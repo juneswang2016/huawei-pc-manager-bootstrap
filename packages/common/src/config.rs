5d924a2a2f9ac8ba4c34a45bad74a58365166bb2
juneswang2016 <juneswang1984@outlook.com>
2024-01-19 21:40:29 +0800

    Update config.rs

--- packages/common/src/config.rs
+++ packages/common/src/config.rs
@@ -15,10 +15,10 @@ pub struct Config {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            manufacturer: "HUAWEI".to_string(),
-            product_name: "HKD-WXX".to_string(),
-            version: "1.0".to_string(),
-            sn: "5EKPM18320000397".to_string(),
+            manufacturer: "HONOR".to_string(),
+            product_name: "KPL-W0X".to_string(),
+            version: "1.19".to_string(),
+            sn: "HPRPM18C25001070".to_string(),
             sku: "C233".to_string(),
         }
     }
